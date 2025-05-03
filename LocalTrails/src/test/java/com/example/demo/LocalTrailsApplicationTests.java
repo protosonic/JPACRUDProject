bc385393bb8c089e3fddb5b89a66840dfb916bf7
protosonic <ndebruin29@gmail.com>
2025-05-02 18:18:02 -0600

    create Spring Boot MVC project, configure dependencies

---
+++ LocalTrails/src/test/java/com/example/demo/LocalTrailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalTrailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
